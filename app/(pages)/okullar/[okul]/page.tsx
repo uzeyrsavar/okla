b3d3554ba936260dbbbdd0afb034c434e08d19a7
uzeyrsavar <uzeyrsavar31@gmail.com>
2026-01-31 13:58:30 +0300

    dynamic

--- app/(pages)/okullar/[okul]/page.tsx
+++ app/(pages)/okullar/[okul]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+export const dynamic = 'force-dynamic'
 import { useEffect, useState } from "react"
 import { SchoolHeader } from "@/app/components/schoolComp/school-header"
 import { SchoolInfo } from "@/app//components/schoolComp/school-info"
