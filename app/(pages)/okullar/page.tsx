a52dae654d6ff1e59feaacc439c6fb73318b7a22
uzeyrsavar <uzeyrsavar31@gmail.com>
2026-01-31 14:00:18 +0300

    dynamic v2

--- app/(pages)/okullar/page.tsx
+++ app/(pages)/okullar/page.tsx
@@ -1,5 +1,5 @@
-'use client'
 export const dynamic = 'force-dynamic'
+
 import React, { useCallback, useEffect, useState } from "react"
 import { Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
